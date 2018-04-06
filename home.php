522cd5fc2911c91f326906a930792b7fa7d12d65
Quinten <31628811+puffballl@users.noreply.github.com>
2018-04-06 09:01:07 +0200

    met foto, zonder (werkende) css

--- home.php
+++ home.php
@@ -1,1 +1,35 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="stylehome.css">
+</head>
+<body>
+
+<div id="navBalk">
+<br>
+<h1>Bauhouse</h1>
+<br>
+</div>
+
+<div id="fotos">
+
+    <img src="media/blurblur.jpg" alt= "fotos">
+    <img src="media/bleach%20drinker.jpg" alt= "fotos">
+    <img src="media/pablo.jpg" alt= "fotos">
+    <img src="media/coolscool.png" alt= "fotos">
+    <img src="media/trupy.png" alt= "fotos">
+    <img src="media/cluster%20harold.jpg" alt= "fotos">
+    <img src="media/deprest%20mexican.jpg" alt= "fotos">
+    <img src="media/to%20spicy%20for%20u.jpg" alt= "fotos">
+    <img src="media/white%20face.jpg" alt= "fotos">
+    <img src="media/mad%20mexican.jpg" alt= "fotos">
+
+</div>
+
+</body>
+</html>
