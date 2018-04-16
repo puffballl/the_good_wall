cae7d8faeda21dfcffb8d83d372d4a7d977402a5
MorrisonLek <34651267+MorrisonLek@users.noreply.github.com>
2018-04-16 09:58:57 +0200

    Add files via upload

---
+++ log in/wachtwoordvergeten.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Wachtwoord Vergeten</title>
+</head>
+<body>
+
+
+
+</body>
+</html>
+
+<?php
+$dbc = mysqli_connect('localhost','Morrison_L', 'wachtwoord','24935_db') or die ('nee');
+?>
---
+++ upload/upload.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<link rel="stylesheet" type="text/css" href="upload.css">
+  <head>
+    <meta charset="utf-8">
+    <title>upload</title>
+  </head>
+  <body>
+<div class="content">
+
+  <div id="taakbalk"><a href="home.php"><img src="images/Logo.png" alt="Logo"></a></div>
+
+    <br> <br> <br> <br>
+    <center><font face = "arial black" size = "2"><h1>Uploaden </h1></font/></center> <br>
+
+    <div id="upload">
+    <input type="file" name="" id="fileToUpload">
+    <center><font face = "arial black" size = "2"> <br> <br> <br> <h2>Sleep hier uw plaatjes in <br> om te uploaden! <br><br><br><br><br><br> Of klik hier!</h2></font/></center> <br> <br>
+    <input type="submit" value="Upload" name="submit"> </div>
+
+</div>
+  </body>
+</html>
