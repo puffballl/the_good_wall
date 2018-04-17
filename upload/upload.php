04083679cbc3370916078f44fb83a219d5cc105b
Quinten <31628811+puffballl@users.noreply.github.com>
2018-04-17 08:45:46 +0200

    eyo

--- upload/upload.php
+++ upload/upload.php
@@ -12,12 +12,13 @@
 
     <br> <br> <br> <br>
     <center><font face = "arial black" size = "2"><h1>Uploaden </h1></font/></center> <br>
-
+    <form action="verwerk_upload.php" method="post" enctype="multipart/form-data">
     <div id="upload">
-    <input type="file" name="" id="fileToUpload">
-    <center><font face = "arial black" size = "2"> <br> <br> <br> <h2>Sleep hier uw plaatjes in <br> om te uploaden! <br><br><br><br><br><br> Of klik hier!</h2></font/></center> <br> <br>
-    <input type="submit" value="Upload" name="submit"> </div>
-
+    <input type="file" name="image" id="image">
+    <center><font face = "arial black" size = "2"> <br> <br> <br> <h2>Sleep hier uw plaatjes in <br> om te uploaden! <br><br><br><input type="text" cols="40" rows="4" placeholder="Een beetje extra caracter bij het plaatje." <br><br><br> Of klik hier!</h2></font/></center> <br> <br>
+    <input type="submit" value="Upload" name="submit">
+    </div>
+    </form>
 </div>
   </body>
 </html>
