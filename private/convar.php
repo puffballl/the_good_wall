7378b8c94fd34009d6235b5bb8e0b93eb5bcdce4
Quinten <31628811+puffballl@users.noreply.github.com>
2018-11-20 19:15:00 +0100

    Update convar.php

--- private/convar.php
+++ private/convar.php
@@ -1,8 +1,4 @@
 <?php
 
-define('HOST','localhost');
-define('USER','Quinten_R');
-define('PASS','Mkhf.');
-define('DBNAME','24935_db');
-$mysqli = new mysqli(HOST, USER, PASS, DBNAME) or die ('Error connecting');
+prive
 
